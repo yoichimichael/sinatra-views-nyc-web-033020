215f5a39824bbd959a6df7872449be50ffc9f473
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-04-20 14:23:29 -0700

    Done.

--- app.rb
+++ app.rb
@@ -3,6 +3,13 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 	get '/' do
-		
-	end
+    #"<h1>Hello World</h1>"
+    erb :index #<-- knows to look for file in views directory
+  end
+  
+  get "/info" do
+    #"Testing the info page"
+    erb :info #<-- separate views file
+  end
+
 end
